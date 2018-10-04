69a3a87830bd7235908345775be4efb0ae038fef
Luis Blázquez Miñambres <40697133+luisblazquezm@users.noreply.github.com>
2018-10-04 21:11:58 +0200

    Delegados Voces Idiomas

--- Sesion2/Delegados2/Delegados2/Controller.h
+++ Sesion2/Delegados2/Delegados2/Controller.h
@@ -11,17 +11,28 @@
 // Convierte el Controlador en el delegado del objeto NSSpeechSynthesizer
 // Añadiendo NSSpeechSynthesizerDelegate
 
-@interface Controller : NSObject <NSSpeechSynthesizerDelegate,NSWindowDelegate>
+@interface Controller : NSObject <NSSpeechSynthesizerDelegate,
+                                  NSWindowDelegate,
+                                  NSTableViewDelegate,
+                                  NSTableViewDataSource>
 {
     IBOutlet NSTextField *entrada;
     NSSpeechSynthesizer *speechSynth;
     IBOutlet NSButton *stopButton;
     IBOutlet NSButton *sayButton;
+    IBOutlet NSTableView *voiceTable;
+    NSArray *voices;
 }
 
+// Comportamiento speak line
 -(IBAction)sayIt:(id)sender;
 -(IBAction)stopIt:(id)sender;
 -(void)speechSynthesizer:(NSSpeechSynthesizer *)sender
        didFinishSpeaking:(BOOL)finishedSpeaking;
+
+// Comportamiento cierre ventana
+-(BOOL)windowShouldClose:(NSWindow *)sender;
+
+// Comportamiento diferentes voces
 
 @end
--- Sesion2/Delegados2/Delegados2/Controller.m
+++ Sesion2/Delegados2/Delegados2/Controller.m
@@ -17,6 +17,7 @@
         NSLog(@"En init\r");
         speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil]; // Se inicia con la voz e idioma por defecto de mi sistema
         [speechSynth setDelegate:self]; // Convierte el Controlador en el delegado del objeto NSSpeechSynthesizer
+        voices = [NSSpeechSynthesizer availableVoices];
     }
     NSLog(@"Self en init es: %@", self);
     return self;
@@ -32,6 +33,7 @@
     }
     [stopButton setEnabled:YES];
     [sayButton setEnabled:NO];
+    [voiceTable setEnabled:NO];
     [speechSynth startSpeakingString:string];
     NSLog(@"Comienza el dictado de la cadena %@\r", string);
 }
@@ -47,7 +49,61 @@
 {
     [stopButton setEnabled:NO];
     [sayButton setEnabled:YES];
+    [voiceTable setEnabled:YES];
     NSLog(@"Terminado: %d", finishedSpeaking);
 }
 
+-(BOOL)windowShouldClose:(NSWindow *)sender
+{
+    NSInteger respuesta;
+    
+    respuesta = NSRunAlertPanel(@"Atencion",
+                               @"¿Está seguro de que desea cerrar la ventana?.Esta accion finalizará la ejecución de la aplicación ",
+                               @"No",
+                               @"Si",
+                               nil);
+    if(respuesta == NSAlertDefaultReturn)
+        return NO;
+    else
+        [NSApp terminate:self];
+        return YES;
+    
+}
+
+-(void) awakeFromNib
+{
+    NSString *defaultVoice = [NSSpeechSynthesizer defaultVoice];
+    NSInteger defaultRow = [voices indexOfObject:defaultVoice];
+    NSIndexSet *defaultRowIndex = [[NSIndexSet alloc]
+                                   initWithIndex:defaultRow];
+    
+    [voiceTable selectRowIndexes:defaultRowIndex
+            byExtendingSelection:NO];
+    [voiceTable scrollRowToVisible:defaultRow];
+}
+
+-(NSInteger) numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return [voices count];
+}
+
+-(id) tableView:(NSTableView *)tableView
+objectValueForTableColumn:(NSTableColumn *)tableColumn
+            row:(NSInteger)row
+{
+    NSString *v = [voices objectAtIndex:row];
+    NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:v];
+    return [dict objectForKey:NSVoiceAge];
+}
+
+-(void) tableViewSelectionDidChange:(NSNotification *)notification
+{
+    NSInteger row = [voiceTable selectedRow];
+    if (row == -1)
+        return;
+    
+    NSString *selectedVoice = [voices objectAtIndex:row];
+    [speechSynth setVoice:selectedVoice];
+    NSLog(@"Voz seleccionada = %@\r", selectedVoice);
+}
 @end
