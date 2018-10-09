16405893ece947bdb412c3a5c85f0cd079a7766d
Luis Blázquez Miñambres <40697133+luisblazquezm@users.noreply.github.com>
2018-10-09 20:18:15 +0200

    Paneles

---
+++ Sesion4/Paneles y otras ventanas/EjercicioTablasTests/EjercicioTablasTests.m
@@ -0,0 +1,39 @@
+//
+//  EjercicioTablasTests.m
+//  EjercicioTablasTests
+//
+//  Created by alumno5 on 8/10/18.
+//  Copyright © 2018 USAL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EjercicioTablasTests : XCTestCase
+
+@end
+
+@implementation EjercicioTablasTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
