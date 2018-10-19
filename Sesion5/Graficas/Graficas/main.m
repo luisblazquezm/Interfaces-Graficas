a50bfb0322f14ece0c03de7d98e89d657177d4ab
Luis Blázquez Miñambres <40697133+luisblazquezm@users.noreply.github.com>
2018-10-19 15:31:50 +0200

    Commit

---
+++ Sesion5/Graficas/Graficas/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Graficas
+//
+//  Created by Luis Blazquez Miñambres on 19/10/18.
+//  Copyright © 2018 Luis Blazquez Miñambres. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
