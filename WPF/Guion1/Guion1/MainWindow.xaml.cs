b45958eeac9f6a5e99080e4049f6ca8fce693777
luisblazquezm <luisblazquezm@usal.es>
2018-11-06 17:07:57 +0100

    Commit 06/11
    - Guion 1 de Practicas Realizado

---
+++ WPF/Guion1/Guion1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Guion1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonInvierte_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = CajaTexto;
+            Label etiq = Etiqueta;
+            int i;
+            string aux = "";
+            for (i = 0; i < tb.Text.Length; i++)
+                aux += tb.Text[tb.Text.Length - i - 1];
+
+            etiq.Content = aux;
+        }
+
+        private void BotonMayuscula_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = CajaTexto;
+            Label etiq = Etiqueta;
+            etiq.Content = CajaTexto.Text.ToUpper();
+        }
+
+    }
+}
