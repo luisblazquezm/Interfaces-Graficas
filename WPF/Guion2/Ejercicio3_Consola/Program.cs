22401bc11a6fb36128268943f444481678f5f1be
luisblazquezm <luisblazquezm@usal.es>
2018-11-15 11:18:34 +0100

    Commit Guion2

---
+++ WPF/Guion2/Ejercicio3_Consola/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio3_Consola
+{
+    // Esto está mal hecho ( ALTO ACOPLAMIENTO )
+    class Acopladas
+    {
+        static void Main(string[] args)
+        {
+            observador obj = new observador();
+            obj.funciona();
+        }
+    }
+    class observador
+    {
+        TrabajoDuro tb;
+        public observador() { tb = new TrabajoDuro(this); } // Le pasa la referencia al observador (si mismo)
+        public void funciona()
+        {
+            Console.WriteLine("Vamos a probar el informe");
+            tb.ATrabajar();
+            Console.WriteLine("Terminado");
+        }
+        public void InformeAvance(int x)
+        {
+            string str = String.Format("Ya llevamos el {0}", x); // indice de los elementos {0},{1}...", x, y el 0 sustituira el x y 1 el y
+            Console.WriteLine(str);
+        }
+    }
+
+    class otraClase
+    {
+        // TrabajoDuro miTrabajo = new TrabajoDuro(this); // OJO NO ES REUTILIZABLE (Clase acoplada, necesita una referencia de observador obligatoriamente)
+    }
+
+
+    class TrabajoDuro
+    {
+        int PocentajeHecho;
+        observador eljefe;
+        public TrabajoDuro(observador o) 
+        {
+            PocentajeHecho = 0;
+            eljefe = o;
+        }
+        public void ATrabajar()
+        {
+            int i;
+            for (i = 0; i < 500; i++)
+            {
+                System.Threading.Thread.Sleep(1); //Hacemos el trabajo
+                switch (i)
+                {
+                    case 125:
+                        PocentajeHecho = 25;
+                        eljefe.InformeAvance(PocentajeHecho);
+                        break;
+                    case 250:
+                        PocentajeHecho = 50;
+                        eljefe.InformeAvance(PocentajeHecho);
+                        break;
+                    case 375:
+                        PocentajeHecho = 75;
+                        eljefe.InformeAvance(PocentajeHecho);
+                        break;
+                }
+            }
+        }
+    }
+    
+}
---
+++ WPF/Guion2/Ejercicio3_Consola2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio3_Consola2
+{
+    // Esto está mal hecho ( ALTO ACOPLAMIENTO )
+    class Acopladas
+    {
+        static void Main(string[] args)
+        {
+            observador obj = new observador();
+            obj.funciona();
+        }
+    }
+
+
+    /// <summary>
+    ///  OJO EL EVENTO INDICA MEDIANTE UN EVENTO DEL MODELO A LA VISTA (QUE ESTAS SE ACTUALICEN) CUANDO EL MODELO CAMBIE
+    /// </summary>
+
+    class observador
+    {
+        TrabajoDuro tb;
+        public observador() { tb = new TrabajoDuro(); } // Le pasa la referencia al observador (si mismo)
+        public void funciona()
+        {
+            Console.WriteLine("Vamos a probar sin Informe");
+            tb.ATrabajar();
+            Console.WriteLine("Vamos a probar con Informe1");
+            tb.tareaEnProceso += InformeAvance; // Aporta más flexibilidad
+            tb.ATrabajar();//<--------------------------------- OJO en esta ejecuta solo InformeAvance 
+            Console.WriteLine("Probando probando, cambio y corto.");
+            tb.tareaEnProceso += InformeAvance2; // Aporta más flexibilidad
+            //tb.tareaEnProceso -= InformeAvance; // No quiero que se ejecute INforme avance
+            tb.ATrabajar(); //<--------------------------------- OJO en esta ejecuta los dos InformeAvance e InformeAvance2
+            Console.WriteLine("Terminado");
+        }
+        public void InformeAvance(Object sender, TareasEventArgs e)
+        {
+            string str = String.Format("Ya llevamos el {0}", e.x); // indice de los elementos {0},{1}...", x, y el 0 sustituira el x y 1 el y
+            Console.WriteLine(str);
+        }
+        public void InformeAvance2(Object sender, TareasEventArgs e)
+        {
+            string str = String.Format("*****"); 
+            Console.WriteLine(str);
+        }
+    }
+
+    class otraClase
+    {
+        TrabajoDuro miTrabajo = new TrabajoDuro(); 
+    }
+
+    // Notación {NOMBRE EVENTO}EventArgs
+    class TareasEventArgs : EventArgs // <----------- Hereda de EventArgs (Es como el NSDictionary de Cocoa)
+    {
+        public int x; // Se puede poner lo que queramos
+    }
+
+
+    delegate void TipoDelegado (Object sender, TareasEventArgs e);
+
+    class TrabajoDuro
+    {
+        int PocentajeHecho;                      
+        public event TipoDelegado tareaEnProceso;
+
+        // Siempre se pone para disparar el evento on{NOMBRE DEL EVENTO}
+        void onTareaEnProceso(TareasEventArgs e)
+        {
+            if (tareaEnProceso != null) // <------------------------ IMPORTANTE para ejecutar un delegado siempre hay que comprobar que no sea NULL
+                tareaEnProceso(this, e);
+        }
+
+       
+        public TrabajoDuro(/*observador o*/) // Clase desacoplada, ya no depende de la clase Obesevador
+        {
+            PocentajeHecho = 0;
+            // eljefe = o;
+        }
+
+        public void ATrabajar() // Metodo que se va a ejecutar
+        {
+            int i;
+            TareasEventArgs e = new TareasEventArgs();
+            for (i = 0; i < 500; i++)
+            {
+                System.Threading.Thread.Sleep(1); //Hacemos el trabajo
+                switch (i)
+                {
+                    case 125:
+                        e.x = 25;
+                        onTareaEnProceso(e);
+                        break;
+                    case 250:
+                        e.x = 50;
+                        onTareaEnProceso(e);
+                        break;
+                    case 375:
+                        e.x = 75;
+                        onTareaEnProceso(e);
+                        break;
+                }
+            }
+        }
+    }
+}
